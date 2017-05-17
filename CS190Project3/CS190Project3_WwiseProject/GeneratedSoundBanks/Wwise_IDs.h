6833471746ae10722bcde2fceeeaababa609cfcc
belliott3 <belliott3@live.com>
2017-05-17 14:16:44 -0700

    Sound update
    Attempt at adding in Wwise stuff on my end. Unity being janky, though.

---
+++ CS190Project3/CS190Project3_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,35 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID AMBIENCE = 85412153U;
+        static const AkUniqueID FOOTSTEPS = 2385628198U;
+        static const AkUniqueID WOLVES = 3324770629U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID BHE_BANK = 2087041853U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+    } // namespace BUSSES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
