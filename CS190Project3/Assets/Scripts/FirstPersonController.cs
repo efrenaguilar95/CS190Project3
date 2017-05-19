789474212dcc38f1821cfab67ce8daa947aec714
efrenaguilar95 <efrenaguilar95@hotmail.com>
2017-05-18 18:24:06 -0700

    Fixed Leaf Sounds

--- CS190Project3/Assets/Scripts/FirstPersonController.cs
+++ CS190Project3/Assets/Scripts/FirstPersonController.cs
@@ -23,9 +23,6 @@ namespace UnityStandardAssets.Characters.FirstPerson
         [SerializeField] private CurveControlledBob m_HeadBob = new CurveControlledBob();
         [SerializeField] private LerpControlledBob m_JumpBob = new LerpControlledBob();
         [SerializeField] private float m_StepInterval;
-        [SerializeField] private AudioClip[] m_FootstepSounds;    // an array of footstep sounds that will be randomly selected from.
-        [SerializeField] private AudioClip m_JumpSound;           // the sound played when character leaves the ground.
-        [SerializeField] private AudioClip m_LandSound;           // the sound played when character touches back on ground.
 
         private Camera m_Camera;
         private bool m_Jump;
@@ -85,9 +82,9 @@ namespace UnityStandardAssets.Characters.FirstPerson
 
         private void PlayLandingSound()
         {
-            m_AudioSource.clip = m_LandSound;
-            m_AudioSource.Play();
-            m_NextStep = m_StepCycle + .5f;
+            //m_AudioSource.clip = m_LandSound;
+            //m_AudioSource.Play();
+            //m_NextStep = m_StepCycle + .5f;
         }
 
 
@@ -135,8 +132,8 @@ namespace UnityStandardAssets.Characters.FirstPerson
 
         private void PlayJumpSound()
         {
-            m_AudioSource.clip = m_JumpSound;
-            m_AudioSource.Play();
+            //m_AudioSource.clip = m_JumpSound;
+            //m_AudioSource.Play();
         }
 
 
---
+++ CS190Project3/Assets/Scripts/TreeAmbience.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeAmbience : MonoBehaviour {
+
+    private int stop = 0;
+
+	// Use this for initialization
+	void Start () {
+        AkSoundEngine.PostEvent("Leaves", this.gameObject);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (stop == 0)
+        {
+            stop++;
+            AkSoundEngine.PostEvent("Leaves", this.gameObject);
+        }
+
+        }
+}
