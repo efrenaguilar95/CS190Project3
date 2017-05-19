cdcde819b150937e8003b88f6b2790c37d23127f
efrenaguilar95 <efrenaguilar95@hotmail.com>
2017-05-18 18:01:23 -0700

    Fixed Footsteps

--- CS190Project3/Assets/Scripts/FirstPersonController.cs
+++ CS190Project3/Assets/Scripts/FirstPersonController.cs
@@ -7,7 +7,6 @@ using Random = UnityEngine.Random;
 namespace UnityStandardAssets.Characters.FirstPerson
 {
     [RequireComponent(typeof (CharacterController))]
-    [RequireComponent(typeof (AudioSource))]
     public class FirstPersonController : MonoBehaviour
     {
         [SerializeField] private bool m_IsWalking;
@@ -166,16 +165,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
             {
                 return;
             }
-            // pick & play a random footstep sound from the array,
-            // excluding sound at index 0
-
-
-//            int n = Random.Range(1, m_FootstepSounds.Length);
-//            m_AudioSource.clip = m_FootstepSounds[n];
-//            m_AudioSource.PlayOneShot(m_AudioSource.clip);
-//            // move picked sound to index 0 so it's not picked next time
-//            m_FootstepSounds[n] = m_FootstepSounds[0];
-//            m_FootstepSounds[0] = m_AudioSource.clip;
+            AkSoundEngine.PostEvent("Footsteps", this.gameObject);
         }
 
 
