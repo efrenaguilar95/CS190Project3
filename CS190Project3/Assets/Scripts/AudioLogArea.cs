6cb7dff9d8aa52f4cec268da061611597d11dda8
Jake Smith <jesmith24@comcast.net>
2017-06-12 00:43:50 -0700

    character movespeed can be nerfed

--- CS190Project3/Assets/Scripts/AudioLogArea.cs
+++ CS190Project3/Assets/Scripts/AudioLogArea.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class AudioLogArea : MonoBehaviour {
 
@@ -8,21 +9,35 @@ public class AudioLogArea : MonoBehaviour {
     public string optionalAudioLogEventName;
     public bool hasOptional;
     public bool isHouse;
+    public float duration = 10f;
     private bool played;
+
+    public UnityStandardAssets.Characters.FirstPerson.FirstPersonController controller;
 
     void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag.Equals("Player") && !played)
         {
             played = true;
+            /*
             if (hasOptional && GameManager.visitedHouse)
                 AkSoundEngine.PostEvent(optionalAudioLogEventName, other.gameObject);
             else
                 AkSoundEngine.PostEvent(audioLogEventName, other.gameObject);
             if (isHouse)
                 GameManager.visitedHouse = true;
-
+            */
             GameManager.numCollected++;
+
+            StartCoroutine(nerfSpeed(duration));
         }
+    }
+
+    IEnumerator nerfSpeed(float duration)
+    {
+        controller = GameObject.Find("FPSController").GetComponent<FirstPersonController>();
+        controller.m_WalkSpeed = (2);
+        yield return new WaitForSeconds(duration);
+        controller.m_WalkSpeed = (10);
     }
 }
