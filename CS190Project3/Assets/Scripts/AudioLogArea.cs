855433d1a17e09d7465d72428ad9a397dfb95a39
Avi Miller <TrebecS@Gmail.com>
2017-06-12 17:16:13 -0700

    Added lights out functionality

--- CS190Project3/Assets/Scripts/AudioLogArea.cs
+++ CS190Project3/Assets/Scripts/AudioLogArea.cs
@@ -10,26 +10,36 @@ public class AudioLogArea : MonoBehaviour {
     public bool hasOptional;
     public bool isHouse;
     public float duration = 10f;
+	public bool lightsOut = false;
+	public float fadeTime = 3f;
     private bool played;
+	private float baseIntensity;
+
+	[SerializeField] Light lightObj;
 
     public UnityStandardAssets.Characters.FirstPerson.FirstPersonController controller;
+
+	void Start(){
+		if (lightsOut)
+			baseIntensity = lightObj.intensity;
+	}
 
     void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag.Equals("Player") && !played)
         {
             played = true;
-            /*
             if (hasOptional && GameManager.visitedHouse)
                 AkSoundEngine.PostEvent(optionalAudioLogEventName, other.gameObject);
             else
                 AkSoundEngine.PostEvent(audioLogEventName, other.gameObject);
             if (isHouse)
                 GameManager.visitedHouse = true;
-            */
             GameManager.numCollected++;
 
             StartCoroutine(nerfSpeed(duration));
+			if(lightsOut)
+				StartCoroutine(LightsOut(duration));
         }
     }
 
@@ -42,4 +52,12 @@ public class AudioLogArea : MonoBehaviour {
         controller.m_WalkSpeed = (3);
 		controller.m_RunSpeed = (4);
     }
+
+	IEnumerator LightsOut(float duration){
+		yield return new WaitForSeconds(duration);
+		for(float i = 0; i < fadeTime; i+=0.5f){
+			lightObj.intensity = Mathf.Lerp(baseIntensity, 0, i/fadeTime);
+			yield return new WaitForSeconds(0.5f);
+		}
+	}
 }
